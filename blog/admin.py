d23be96bfcfeb68695894b1521452191656e8d02
Abdul Hasib Yousufzai <93597241+abdulhasib9@users.noreply.github.com>
2025-01-10 08:53:12 -0800

    registered all the models in admin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser, Category, Tag, Post, PostImage, PostCategory, PostTag, Comment, Reply
 
-# Register your models here.
+
+class CustomUserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'profile_picture', 'bio')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageInline]
+    list_display = ('title', 'user', 'created_at', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'created_at')
+    search_fields = ('post__title', 'user__username', 'content')
+
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'user', 'created_at')
+    search_fields = ('comment__content', 'user__username', 'content')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Reply, ReplyAdmin)
